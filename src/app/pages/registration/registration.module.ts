70206e3832b306cb6775b1c6a01373700f883f8c
zalmajali <z.almajali@taqnyat.sa>
2022-12-22 12:53:04 +0300

    initial commit

---
+++ src/app/pages/registration/registration.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegistrationPageRoutingModule } from './registration-routing.module';
+
+import { RegistrationPage } from './registration.page';
+import {PolicycompComponent} from "../policycomp/policycomp.component";
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RegistrationPageRoutingModule
+    ],
+    declarations: [RegistrationPage, PolicycompComponent]
+})
+export class RegistrationPageModule {}
---
+++ src/app/pages/sendpoint/sendpoint.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SendpointPageRoutingModule } from './sendpoint-routing.module';
+
+import { SendpointPage } from './sendpoint.page';
+import {AlertdataComponent} from "../alertdata/alertdata.component";
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        SendpointPageRoutingModule
+    ],
+    declarations: [SendpointPage, AlertdataComponent]
+})
+export class SendpointPageModule {}
